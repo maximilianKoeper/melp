8c1af885f0b6dee40c06687e51a8bf6a5d00cc66
maximilianKoeper <16453175+maximilianKoeper@users.noreply.github.com>
2021-09-20 18:33:18 +0200

    bugfix

--- melp/multithreading.py
+++ melp/multithreading.py
@@ -16,7 +16,7 @@ def mt_rate(npz, binned, i):
     calls = melp.TileHitRate(input_files[i], output_files[i], output_files[i]+"edep")
     print("read file ", i+1)
     print("started thread ", i+1)
-    calls.tileHitRateHID()  
+    calls.tileHitRateHID()
     if npz == True:
         calls.saveNpz()
     if binned == True:
@@ -27,14 +27,14 @@ def mt_angle(txt, npz, binned, angle, i):
     calls = melp.TileHitAngle(input_files[0][i], output_files[i])
     print("read file ", i+1)
     print("started thread ", i+1)
-    calls.hitAngleTID(angle)
+    calls.hitAngleTID(angle=angle)
     if txt == True:
         calls.saveTxt()
     if npz == True:
         calls.saveNpz()
     if binned == True:
         calls.saveBinned()
-    
+
 
 def mt_angleHelix(txt, npz, binned, i):
     calls = melp.TileHitAngle(input_files[0][i], output_files[i])
@@ -48,7 +48,7 @@ def mt_angleHelix(txt, npz, binned, i):
     if binned == True:
         calls.saveBinned()
 
-    
+
 # defining which function to run
 def run_mt(function_str, src, args): #set func to the one you want to use from above and pass args
     # set used threads
@@ -62,13 +62,13 @@ def run_mt(function_str, src, args): #set func to the one you want to use from a
     # get list of input files
     input_files.append(glob(src)) #set to directory with .root files you want to analyse
 
-    # generate list of output files 
+    # generate list of output files
     for j in range(len(input_files[0])):
         output_files.append("out"+str(j+1))
 
 
     # map multiprocessing pool
-    pool = mp.Pool(mp.cpu_count() - unused_threads) 
+    pool = mp.Pool(mp.cpu_count() - unused_threads)
     if function_str == "mt_rate":
         func = partial(mt_rate, *args)
         pool.map(func, [i for i in range(len(input_files[0]))])
@@ -77,7 +77,7 @@ def run_mt(function_str, src, args): #set func to the one you want to use from a
         func = partial(mt_angle, *args)
         pool.map(func, [i for i in range(len(input_files[0]))])
 
-    elif function_str == "mt_angleHelix": 
+    elif function_str == "mt_angleHelix":
         func = partial(mt_angleHelix, *args)
         pool.map(func, [i for i in range(len(input_files[0]))])
 
@@ -86,5 +86,3 @@ def run_mt(function_str, src, args): #set func to the one you want to use from a
 
 #if __name__ == "__main__":
 #  run_mt()
-
-
