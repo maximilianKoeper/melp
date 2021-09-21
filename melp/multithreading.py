ff34275f295551b414b0dc4a36f5b9decb866196
maximilianKoeper <16453175+maximilianKoeper@users.noreply.github.com>
2021-09-21 14:15:18 +0200

    close pool at the end of run_mt

--- melp/multithreading.py
+++ melp/multithreading.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 #defining the functions with multithreading support
-#Arguments: 
+#Arguments:
     #-txt, npz, binned: bool, if true it saves in that format
     #-angle: str, "norm" = angle between hit direction and normal vector of tile, "theta" = polar angle, "phi" = azimuth angle
 def mt_tileHitRateHID(input_files, output_files, npz, i):
@@ -47,7 +47,7 @@ def mt_hitAngleHelix(input_files, output_files, txt, npz, binned, i):
 
 
 
-def run_mt(function_str, src, args): 
+def run_mt(function_str, src, args):
 #function_str: pass function as str, src: directory of root files to analyse, args: arguments for parallelized function
     # set used threads
     unused_threads = 2 #set the number of threads you don't want to use
@@ -61,7 +61,7 @@ def run_mt(function_str, src, args):
 
 
     # get list of input files
-    input_files.append(glob(src)) 
+    input_files.append(glob(src))
 
     # generate list of output files
     for j in range(len(input_files[0])):
@@ -73,15 +73,13 @@ def run_mt(function_str, src, args):
     if function_str == "mt_tileHitRateHID":
         func = partial(mt_tileHitRateHID, input_files, output_files, args)
         pool.map(func, [i for i in range(len(input_files[0]))])
-        pool.close()
 
     elif function_str == "mt_hitAngleTID":
         func = partial(mt_hitAngleTID, input_files, output_files, *args)
         pool.map(func, [i for i in range(len(input_files[0]))])
-        pool.close()
 
     elif function_str == "mt_hitAngleHelix":
         func = partial(mt_hitAngleHelix, input_files, output_files, *args)
         pool.map(func, [i for i in range(len(input_files[0]))])
-        pool.close()
 
+    pool.close()
