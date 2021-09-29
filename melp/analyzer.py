e31bdd34318dbca11a8f3c1279281cefec38fa5b
Maximilian Köper <maximilian.koeper@kip.uni-heidelberg.de>
2021-09-29 11:51:34 +0200

    Add HitRate

--- melp/analyzer.py
+++ melp/analyzer.py
@@ -2,6 +2,10 @@ import ROOT
 
 from melp.src.hit import Hit
 
+#---------------------------------------------------------------------
+#  HIT RATE
+#---------------------------------------------------------------------
+
 class HitRate ():
     def __init__ (self, detecor, *args):
         self.Detecor = detecor
@@ -12,7 +16,7 @@ class HitRate ():
         ttree_mu3e_mc = file.Get("mu3e_mchits")
         ttree_mu3e.GetEntry(0)
         if ttree_mu3e.run in self.Detecor.AddedRuns:
-            print("ERROR: RUN already loaded into Detecor")
+            print("ERROR: RUN ", ttree_mu3e.run, " already loaded into Detecor")
             return
         else:
             self.Detecor.AddedRuns.append(ttree_mu3e.run)
@@ -30,9 +34,53 @@ class HitRate ():
                 tilehit = Hit(edep = edep, mc_i = mc_i, hid = hid)
                 self.Detecor.TileDetector.addHit(tile, tilehit)
 
-    def getHitRate(self):
-        for tile in self.Detecor.TileDetector.tile:
-            pass
+    # hitrate (z_pos, total_hits, primary_hits, secondary_hits, tertiary_hits, edep)
+    def getHitRate(self, tileID = -1):
+        hitrate = [[0],[0],[0],[0],[0],[0]]
+        if tileID == -1:
+            for tile in self.Detecor.TileDetector.tile:
+                hitrate[0].append(self.Detecor.TileDetector.getPos(tile)[2])
+                hitrate[1].append(0)
+                hitrate[2].append(0)
+                hitrate[3].append(0)
+                hitrate[4].append(0)
+                hitrate[5].append(0)
+
+                for hit in self.Detecor.TileDetector.tile[tile].hits:
+
+                    edep = hit.edep
+                    hid  = hit.hid
+
+                    hitrate[1][-1] += 1 # add a hit
+                    hitrate[5][-1] += edep # add edep
+                    if abs(hid) == 1:
+                        hitrate[2][-1] += 1 # add a primary hit
+                    elif abs(hid) == 2:
+                        hitrate[3][-1] += 1 # add secondary hit
+                    elif abs(hid) == 3:
+                        hitrate[4][-1] += 1 # add tertiary hit
+
+            return hitrate
+
+
+        else:
+            hitrate[0][0] = self.Detecor.TileDetector.tile[tileID].pos[2] # z position
+
+            for hit in self.Detecor.TileDetector.tile[tileID].hits:
+
+                edep = hit.edep
+                hid  = hit.hid
+
+                hitrate[1][0] += 1 # add a hit
+                hitrate[5][0] += edep # add edep
+                if abs(hid) == 1:
+                    hitrate[2][0] += 1 # add a primary hit
+                elif abs(hid) == 2:
+                    hitrate[3][0] += 1 # add secondary hit
+                elif abs(hid) == 3:
+                    hitrate[4][0] += 1 # add tertiary hit
+
+            return hitrate
 
 
 class HitAngle ():
