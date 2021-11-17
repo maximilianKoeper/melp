e6e887f8126fd527c772a59381f0d3c929ae229c
estein05 <>
2021-11-17 19:09:05 +0100

    added first functions

--- melp/clustering/misc.py
+++ melp/clustering/misc.py
@@ -1,6 +1,7 @@
 from melp import Detector
 import ROOT
 
+#----------------------------------------
 def hittimes_in_file (filename):
     hittimes = {}
     file = ROOT.TFile(filename)
@@ -20,7 +21,7 @@ def hittimes_in_file (filename):
 
     return hittimes
 
-
+#-------------------------------------------
 def hittimes_in_frame (filename, frame_id):
     hittimes = {}
     file = ROOT.TFile(filename)
@@ -39,5 +40,52 @@ def hittimes_in_frame (filename, frame_id):
 
     return hittimes
 
+#------------------------------------------
+def get_key_for_value(dict, val):
+    for key, value in dict.items():
+        if isinstance(value, list):
+            if val == value[0]:
+                return key
+        else:
+            if val == value:
+                return key
+
+    return "key doesn't exist"
+
+#--------------------------------------------
+def get_tid_file(filename):
+    file = ROOT.TFile(filename)
+    ttree_mu3e = file.Get("mu3e")
+    ttree_mu3e_mc = file.Get("mu3e_mchits")
+    tid = {}
+
+    for frame in range(ttree_mu3e.GetEntries()):
+        ttree_mu3e.GetEntry(frame)
+        for i in range(len(ttree_mu3e.tilehit_tile)):
+            tile = ttree_mu3e.tilehit_tile[i]
+            mc_i = ttree_mu3e.tilehit_mc_i[i]
+            ttree_mu3e_mc.GetEntry(mc_i)
 
 
+            tid[tile] = ttree_mu3e_mc.tid
+
+    return tid
+
+#--------------------------------------------------
+def get_tid_frame(filename, frame):
+    file = ROOT.TFile(filename)
+    ttree_mu3e = file.Get("mu3e")
+    ttree_mu3e_mc = file.Get("mu3e_mchits")
+    tid = {}
+    ttree_mu3e.GetEntry(frame)
+    for i in range(len(ttree_mu3e.tilehit_tile)):
+        tile = ttree_mu3e.tilehit_tile[i]
+        mc_i = ttree_mu3e.tilehit_mc_i[i]
+        ttree_mu3e_mc.GetEntry(mc_i)
+
+
+        tid[tile] = ttree_mu3e_mc.tid
+
+    return tid
+
+#-----------------------------------------------
--- melp/clustering/time_cluster.py
+++ melp/clustering/time_cluster.py
@@ -5,6 +5,38 @@ from melp import Detector
 
 from melp.clustering.misc import*
 
+'''
+def time_clustering_frame(filename,frame,threshold):
+    clusters = {}
+    cluster_counter = 0
+    file = ROOT.TFile(filename)
+    ttree_mu3e = file.Get("mu3e")
+
+    hittimes = hittimes_in_frame (filename, frame)
+
+    for hit_time in hittimes.values():
+        for hit_time_2 in hittimes.values():
+            if abs(hit_time[0] - hit_time_2[0]) < threshold:
+                if cluster_counter in clusters.keys():
+                    for val_arr in clusters.values():
+                        if any(hit_time_2[0] in sl for sl in val_arr):
+                            if [get_key_for_value(hittimes, hit_time_2[0]), hit_time_2[0]] not in clusters.values():
+                                clusters[cluster_counter].append([get_key_for_value(hittimes, hit_time_2[0]), hit_time_2[0]])
+                            else:
+                                continue
+                                
+                        else: 
+                            continue
+                    cluster_counter += 1
+
+                else:
+                    clusters[cluster_counter] = [[get_key_for_value(hittimes, hit_time[0]), hit_time[0]], [get_key_for_value(hittimes, hit_time_2[0]), hit_time_2[0]]]
+
+            else:
+                continue
+
+    return clusters
+'''
 
 def time_clustering_frame(filename,frame,threshold):
     clusters = {}
@@ -13,14 +45,12 @@ def time_clustering_frame(filename,frame,threshold):
     ttree_mu3e = file.Get("mu3e")
 
     hittimes = hittimes_in_frame (filename, frame)
-    for hit_time in hittimes.values():
-        for hit_time_2 in hittimes.values():
-            if (np.array(hit_time) - np.array(hit_time_2)) < threshold:
-                cluster_counter += 1
-                clusters[cluster_counter] = [hit_time, hit_time_2] 
-
-            else:
-                continue
+    i=0 
+    j=i+1
+    for i in range(len(hittimes.values())):
+        for j in range(len(hittimes.values())-i):
+            for key in hittimes.keys():
+                if abs() < threshold:
+                    clusters[cluster_counter] = [[get_key_for_value(hittimes, np.array(hittimes.values())[i]), np.array(hittimes.values())[i]], [get_key_for_value(hittimes, np.array(hittimes.values())[j]), np.array(hittimes.values())[j]]]
 
     return clusters
-
