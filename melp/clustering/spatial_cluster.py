2cedbd78146c3164579eff74c80bf6646902ffc7
Maximilian Koeper <maximilian.koeper@kip.uni-heidelberg.de>
2021-11-18 22:41:20 +0100

    latest commit

--- melp/clustering/spatial_cluster.py
+++ melp/clustering/spatial_cluster.py
@@ -15,8 +15,38 @@ def spatial_truth_clusters_frame(filename,frame,threshold):
     return clusters
 """
 
+def build_mask(filename, frame, mask_type = "medium"): #build mask around hit
+    file = ROOT.TFile(filename)
+    ttree_mu3e = file.Get("mu3e")
+    mask = {}
+    ttree_mu3e.GetEntry(frame)
+    if mask_type == "medium":
+        for i in range(len(ttree_mu3e.tilehit_tile)):
+            mask_tmp = []
+            tile_centre = ttree_mu3e.tilehit_tile[i]
+            tile_centre_top = tile_centre - 1
+            tile_centre_bottom = tile_centre + 1
+            tile_left_top = tile_centre - 53
+            tile_left_centre = tile_centre - 52
+            tile_left_bottom = tile_centre - 51
+            tile_right_top = tile_centre + 51
+            tile_right_centre = tile_centre + 52
+            tile_right_bootom = tile_centre + 53
+            mask_tmp.append(tile_centre)
+            mask_tmp.append(tile_centre_top)
+            mask_tmp.append(tile_centre_bottom)
+            mask_tmp.append(tile_left_top)
+            mask_tmp.append(tile_left_centre)
+            mask_tmp.append(tile_left_bottom)
+            mask_tmp.append(tile_right_top)
+            mask_tmp.append(tile_right_centre)
+            mask_tmp.append(tile_right_bootom)
+            mask[tile_centre] = mask_tmp
 
-  
+    else:
+        print("Fail")
+
+    return mask
 
 
 
