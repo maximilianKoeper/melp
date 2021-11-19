fca3bcd2ef1edd45701cd0baee4da0535b53b9bc
estein05 <89516650+estein05@users.noreply.github.com>
2021-11-19 14:23:19 +0100

    plotted masks in frame

--- melp/clustering/spatial_cluster.py
+++ melp/clustering/spatial_cluster.py
@@ -15,6 +15,7 @@ def spatial_truth_clusters_frame(filename,frame,threshold):
     return clusters
 """
 
+#------------------------------------------
 def build_mask(filename, frame, mask_type = "medium"): #build mask around hit
     file = ROOT.TFile(filename)
     ttree_mu3e = file.Get("mu3e")
@@ -45,7 +46,7 @@ def build_mask(filename, frame, mask_type = "medium"): #build mask around hit
 
     return mask
 
-
+#-----------------------------------------------
 def build_mask_detector_class(filename, frame, mu3e_detector: melp.Detector, mask_type):
     file = ROOT.TFile(filename)
     ttree_mu3e = file.Get("mu3e")
@@ -55,29 +56,33 @@ def build_mask_detector_class(filename, frame, mu3e_detector: melp.Detector, mas
         for i in range(len(ttree_mu3e.tilehit_tile)):
             mask_tmp = []
             tile_centre = ttree_mu3e.tilehit_tile[i]
-            tile_centre_top = mu3e_detector.TileDetector.getNeighbour(tile_centre, "up")
-            tile_centre_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre, "down")
-            tile_left_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "left")
-            tile_right_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "right")
+            tile_centre_top = neighbour_wiper(mu3e_detector, tile_centre, "up")
+            tile_centre_bottom = neighbour_wiper(mu3e_detector, tile_centre, "down")
+            tile_left_centre = neighbour_wiper(mu3e_detector, tile_centre, "left")
+            tile_right_centre = neighbour_wiper(mu3e_detector, tile_centre, "right")
             mask_tmp.append(tile_centre)
             mask_tmp.append(tile_centre_top)
             mask_tmp.append(tile_centre_bottom)
             mask_tmp.append(tile_left_centre)
             mask_tmp.append(tile_right_centre)
+
+            if -1 in mask_tmp:
+                mask_tmp = [x for x in mask_tmp if x != -1]
+
             mask[tile_centre] = mask_tmp
 
     if mask_type == "medium":
         for i in range(len(ttree_mu3e.tilehit_tile)):
             mask_tmp = []
             tile_centre = ttree_mu3e.tilehit_tile[i]
-            tile_centre_top = mu3e_detector.TileDetector.getNeighbour(tile_centre, "up")
-            tile_centre_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre, "down")
-            tile_left_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "left")
-            tile_left_top =  mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "up")
-            tile_left_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "down")
-            tile_right_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "right")
-            tile_right_top = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "up")
-            tile_right_bootom = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "down")
+            tile_centre_top = int(neighbour_wiper(mu3e_detector, tile_centre, "up"))
+            tile_centre_bottom = int(neighbour_wiper(mu3e_detector, tile_centre, "down"))
+            tile_left_centre = int(neighbour_wiper(mu3e_detector, tile_centre, "left"))
+            tile_left_top =  int(neighbour_wiper(mu3e_detector, tile_left_centre, "up"))
+            tile_left_bottom = int(neighbour_wiper(mu3e_detector, tile_left_centre, "down"))
+            tile_right_centre = int(neighbour_wiper(mu3e_detector, tile_centre, "right"))
+            tile_right_top = int(neighbour_wiper(mu3e_detector, tile_right_centre, "up"))
+            tile_right_bootom = int(neighbour_wiper(mu3e_detector, tile_right_centre, "down"))
             mask_tmp.append(tile_centre)
             mask_tmp.append(tile_centre_top)
             mask_tmp.append(tile_centre_bottom)
@@ -87,34 +92,36 @@ def build_mask_detector_class(filename, frame, mu3e_detector: melp.Detector, mas
             mask_tmp.append(tile_right_top)
             mask_tmp.append(tile_right_centre)
             mask_tmp.append(tile_right_bootom)
+            if -1 in mask_tmp:
+                mask_tmp = [x for x in mask_tmp if x != -1]
             mask[tile_centre] = mask_tmp
 
     if mask_type == "big":
         for i in range(len(ttree_mu3e.tilehit_tile)):
             mask_tmp = []
             tile_centre = ttree_mu3e.tilehit_tile[i]
-            tile_centre_top = mu3e_detector.TileDetector.getNeighbour(tile_centre, "up")
-            tile_centre_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre, "down")
-            tile_left_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "left")
-            tile_left_top =  mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "up")
-            tile_left_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "down")
-            tile_right_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "right")
-            tile_right_top = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "up")
-            tile_right_bottom = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "down")
+            tile_centre_top = neighbour_wiper(mu3e_detector, tile_centre, "up")
+            tile_centre_bottom = neighbour_wiper(mu3e_detector, tile_centre, "down")
+            tile_left_centre = neighbour_wiper(mu3e_detector, tile_centre, "left")
+            tile_left_top =  neighbour_wiper(mu3e_detector, tile_left_centre, "up")
+            tile_left_bottom = neighbour_wiper(mu3e_detector, tile_left_centre, "down")
+            tile_right_centre = neighbour_wiper(mu3e_detector, tile_centre, "right")
+            tile_right_top = neighbour_wiper(mu3e_detector, tile_right_centre, "up")
+            tile_right_bottom = neighbour_wiper(mu3e_detector, tile_right_centre, "down")
 
-            tile_centre_far_top = mu3e_detector.TileDetector.getNeighbour(tile_centre_top, "up")
-            tile_centre_far_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre_bottom, "down")
-            tile_left_far_centre = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "left")
-            tile_left_far_top =  mu3e_detector.TileDetector.getNeighbour(tile_left_top, "up")
-            tile_left_far_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_bottom, "down")
-            tile_right_far_centre = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "right")
-            tile_right_far_top = mu3e_detector.TileDetector.getNeighbour(tile_right_top, "up")
-            tile_right_far_bottom = mu3e_detector.TileDetector.getNeighbour(tile_right_bottom, "down")
+            tile_centre_far_top = neighbour_wiper(mu3e_detector, tile_centre_top, "up")
+            tile_centre_far_bottom = neighbour_wiper(mu3e_detector, tile_centre_bottom, "down")
+            tile_left_far_centre = neighbour_wiper(mu3e_detector, tile_left_centre, "left")
+            tile_left_far_top =  neighbour_wiper(mu3e_detector, tile_left_top, "up")
+            tile_left_far_bottom = neighbour_wiper(mu3e_detector, tile_left_bottom, "down")
+            tile_right_far_centre = neighbour_wiper(mu3e_detector, tile_right_centre, "right")
+            tile_right_far_top = neighbour_wiper(mu3e_detector, tile_right_top, "up")
+            tile_right_far_bottom = neighbour_wiper(mu3e_detector, tile_right_bottom, "down")
 
-            tile_far_left_top = mu3e_detector.TileDetector.getNeighbour(tile_left_top, "left")
-            tile_far_left_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_bottom, "left")
-            tile_far_right_top = mu3e_detector.TileDetector.getNeighbour(tile_right_top, "right")
-            tile_far_right_bottom = mu3e_detector.TileDetector.getNeighbour(tile_right_bottom, "right")
+            tile_far_left_top = neighbour_wiper(mu3e_detector, tile_left_top, "left")
+            tile_far_left_bottom = neighbour_wiper(mu3e_detector, tile_left_bottom, "left")
+            tile_far_right_top = neighbour_wiper(mu3e_detector, tile_right_top, "right")
+            tile_far_right_bottom = neighbour_wiper(mu3e_detector, tile_right_bottom, "right")
 
             mask_tmp.append(tile_centre)
             mask_tmp.append(tile_centre_top)
@@ -140,7 +147,13 @@ def build_mask_detector_class(filename, frame, mu3e_detector: melp.Detector, mas
             mask_tmp.append(tile_far_right_top)
             mask_tmp.append(tile_far_right_bottom)
 
+            if -1 in mask_tmp:
+                mask_tmp = [x for x in mask_tmp if x != -1]
+
             mask[tile_centre] = mask_tmp
 
     return mask
 
+#-------------------------------------------
+def build_cluster_mask(filename, frame):
+    pass
