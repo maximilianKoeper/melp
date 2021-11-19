a7341e3fe02f2f0f8643ced35d7533fae4143412
estein05 <89516650+estein05@users.noreply.github.com>
2021-11-19 17:02:53 +0100

    added cluster primaries

--- melp/clustering/spatial_cluster.py
+++ melp/clustering/spatial_cluster.py
@@ -156,6 +156,114 @@ def build_mask_detector_class(filename, frame, mu3e_detector: melp.Detector, mas
 
     return mask
 
-#-------------------------------------------
-def build_cluster_mask(filename, frame):
-    pass
+#------------------------------------------------
+def build_mask_around_cluster_primary(filename, frame, mu3e_detector: melp.Detector, mask_type):
+    primaries = get_cluster_primary_truth_frame(filename, frame)
+
+    file = ROOT.TFile(filename)
+    ttree_mu3e = file.Get("mu3e")
+    mask = {}
+    ttree_mu3e.GetEntry(frame)
+    if mask_type == "small":
+        for i in range(len(primaries)):
+            mask_tmp = []
+            tile_centre = primaries[i]
+            tile_centre_top = mu3e_detector.TileDetector.getNeighbour(tile_centre, "up")
+            tile_centre_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre, "down")
+            tile_left_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "left")
+            tile_right_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "right")
+            mask_tmp.append(tile_centre)
+            mask_tmp.append(tile_centre_top)
+            mask_tmp.append(tile_centre_bottom)
+            mask_tmp.append(tile_left_centre)
+            mask_tmp.append(tile_right_centre)
+
+            if False in mask_tmp:
+                mask_tmp = [x for x in mask_tmp if x != False]
+
+            mask[tile_centre] = mask_tmp
+
+    if mask_type == "medium":
+        for i in range(len(primaries)):
+            mask_tmp = []
+            tile_centre = primaries[i]
+            tile_centre_top = mu3e_detector.TileDetector.getNeighbour(tile_centre, "up")
+            tile_centre_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre, "down")
+            tile_left_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "left")
+            tile_left_top = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "up")
+            tile_left_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "down")
+            tile_right_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "right")
+            tile_right_top = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "up")
+            tile_right_bootom = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "down")
+            mask_tmp.append(tile_centre)
+            mask_tmp.append(tile_centre_top)
+            mask_tmp.append(tile_centre_bottom)
+            mask_tmp.append(tile_left_top)
+            mask_tmp.append(tile_left_centre)
+            mask_tmp.append(tile_left_bottom)
+            mask_tmp.append(tile_right_top)
+            mask_tmp.append(tile_right_centre)
+            mask_tmp.append(tile_right_bootom)
+
+            if False in mask_tmp:
+                mask_tmp = [x for x in mask_tmp if x != False]
+
+            mask[tile_centre] = mask_tmp
+
+    if mask_type == "big":
+        for i in range(len(primaries)):
+            mask_tmp = []
+            tile_centre = primaries[i]
+            tile_centre_top = mu3e_detector.TileDetector.getNeighbour(tile_centre, "up")
+            tile_centre_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre, "down")
+            tile_left_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "left")
+            tile_left_top =  mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "up")
+            tile_left_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "down")
+            tile_right_centre = mu3e_detector.TileDetector.getNeighbour(tile_centre, "right")
+            tile_right_top = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "up")
+            tile_right_bottom = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "down")
+
+            tile_centre_far_top = mu3e_detector.TileDetector.getNeighbour(tile_centre_top, "up")
+            tile_centre_far_bottom = mu3e_detector.TileDetector.getNeighbour(tile_centre_bottom, "down")
+            tile_left_far_centre = mu3e_detector.TileDetector.getNeighbour(tile_left_centre, "left")
+            tile_left_far_top =  mu3e_detector.TileDetector.getNeighbour(tile_left_top, "up")
+            tile_left_far_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_bottom, "down")
+            tile_right_far_centre = mu3e_detector.TileDetector.getNeighbour(tile_right_centre, "right")
+            tile_right_far_top = mu3e_detector.TileDetector.getNeighbour(tile_right_top, "up")
+            tile_right_far_bottom = mu3e_detector.TileDetector.getNeighbour(tile_right_bottom, "down")
+
+            tile_far_left_top = mu3e_detector.TileDetector.getNeighbour(tile_left_top, "left")
+            tile_far_left_bottom = mu3e_detector.TileDetector.getNeighbour(tile_left_bottom, "left")
+            tile_far_right_top = mu3e_detector.TileDetector.getNeighbour(tile_right_top, "right")
+            tile_far_right_bottom = mu3e_detector.TileDetector.getNeighbour(tile_right_bottom, "right")
+
+            mask_tmp.append(tile_centre)
+            mask_tmp.append(tile_centre_top)
+            mask_tmp.append(tile_centre_bottom)
+            mask_tmp.append(tile_left_top)
+            mask_tmp.append(tile_left_centre)
+            mask_tmp.append(tile_left_bottom)
+            mask_tmp.append(tile_right_top)
+            mask_tmp.append(tile_right_centre)
+            mask_tmp.append(tile_right_bottom)
+
+            mask_tmp.append(tile_centre_far_top)
+            mask_tmp.append(tile_centre_far_bottom)
+            mask_tmp.append(tile_left_far_centre)
+            mask_tmp.append(tile_left_far_top)
+            mask_tmp.append(tile_left_far_bottom)
+            mask_tmp.append(tile_right_far_centre)
+            mask_tmp.append(tile_right_far_top)
+            mask_tmp.append(tile_right_far_bottom)
+
+            mask_tmp.append(tile_far_left_top)
+            mask_tmp.append(tile_far_left_bottom)
+            mask_tmp.append(tile_far_right_top)
+            mask_tmp.append(tile_far_right_bottom)
+
+            if False in mask_tmp:
+                mask_tmp = [x for x in mask_tmp if x != False]
+
+            mask[tile_centre] = mask_tmp
+
+    return mask    
