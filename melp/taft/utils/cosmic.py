9d25409c5437acf5994b56806a3463b051727be4
maximilianKoeper <16453175+maximilianKoeper@users.noreply.github.com>
2021-12-02 18:33:16 +0100

    add first work on cosmics

--- melp/taft/utils/cosmic.py
+++ melp/taft/utils/cosmic.py
@@ -2,18 +2,40 @@ import ROOT
 import numpy as np
 
 
+# --------------------------------------------------
+# searches all frames for hits
+# returns time dist from these hits
 def find_cosmic_events(filename: str, **kwargs):
     root_file = ROOT.TFile.Open(filename, "READ")
     ttree_mu3e = root_file.Get(kwargs["ttree_loc"])
 
     frame_with_hit_counter = 0
-    time = []
+    time_total = []
+    time_dist_betw_stations = []
     for frame in range(ttree_mu3e.GetEntries()):
         ttree_mu3e.GetEntry(frame)
 
+        if frame % 100000 == 0:
+            print(frame/ttree_mu3e.GetEntries())
+
         if len(ttree_mu3e.tilehit_tile) > 0:
             frame_with_hit_counter += 1
-            time.append(sum(ttree_mu3e.tilehit_time)/len(ttree_mu3e.tilehit_time) - min(ttree_mu3e.tilehit_time))
+            time_total.append(sum(ttree_mu3e.tilehit_time) / len(ttree_mu3e.tilehit_time) - min(ttree_mu3e.tilehit_time))
+
+            tmp_arr_1 = []
+            tmp_arr_2 = []
+            for hit_index in range(len(ttree_mu3e.tilehit_tile)):
+                tile_id = ttree_mu3e.tilehit_tile[hit_index]
+                if tile_id > 300000:
+                    tmp_arr_2.append(ttree_mu3e.tilehit_time[hit_index])
+                else:
+                    tmp_arr_1.append(ttree_mu3e.tilehit_time[hit_index]+3)
+            if len(tmp_arr_1) > 0 and len(tmp_arr_2) > 0:
+                global_time = min(ttree_mu3e.tilehit_time)
+                tmp_time_1 = sum(tmp_arr_1)/len(tmp_arr_1) - global_time
+                tmp_time_2 = sum(tmp_arr_2)/len(tmp_arr_2) - global_time
+
+                time_dist_betw_stations.append(tmp_time_1-tmp_time_2)
 
     print(frame_with_hit_counter, " von ", ttree_mu3e.GetEntries())
-    return np.array(time)
+    return np.array(time_total), np.array(time_dist_betw_stations)
