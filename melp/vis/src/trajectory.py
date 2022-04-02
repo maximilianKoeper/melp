Bugfix melp.vis (theta calculation) + add muons + vis.select_only_particle_types(list)

melp/vis/src/trajectory.py
@@ -37,7 +37,7 @@ class Trajectory:
 
     def get_helix_path(self):
         x, y, z = [], [], []
-        if self.get_particle_type() in [1,2]:
+        if self.get_particle_type() in [1, 2, 3, 4]:
             x, y, z = self._get_e_helix_path_()
         elif self.get_particle_type() == 0:
             x, y, z = self._get_photon_path_()
@@ -50,9 +50,9 @@ class Trajectory:
         # calculate radius for helix
         radius = self._get_helix_radius_()
 
-        # calculate theta for helix
-        theta = mf.angle_between(np.array([self.px, self.py, self.pz]), np.array([0, 0, 1])) - 90
-        if self.get_particle_type() == 2:
+        # calculate theta for helix (angle towards z direction)
+        theta = abs(np.arctan2(self.pz, self.get_pt()))
+        if self.pz < 0:
             theta *= -1
 
         # calculate middle point of helices
@@ -67,7 +67,10 @@ class Trajectory:
         phi_offset = np.deg2rad(-phi + 270)
 
         # calculating helix path
-        x, y, z = self._get_helix_(hx, hy, hz, radius, theta, phi_offset, 2)
+        if self.get_particle_type() in [3, 4]:  # MU+-
+            x, y, z = self._get_helix_(hx, hy, hz, radius, theta, phi_offset, 3)
+        elif self.get_particle_type() in [1, 2]:  # e+-
+            x, y, z = self._get_helix_(hx, hy, hz, radius, theta, phi_offset, 2)
         return x, y, z
 
     def _get_photon_path_(self):
@@ -90,6 +93,10 @@ class Trajectory:
             charge_q = +1
         elif self.get_particle_type() == 2:  # electron
             charge_q = -1
+        elif self.get_particle_type() == 3:  # Mu+
+            charge_q = +1
+        elif self.get_particle_type() == 4:  # Mu-
+            charge_q = -1
         else:
             return np.NaN
 
@@ -106,6 +113,6 @@ class Trajectory:
 
         x = abs(r) * np.cos(-np.sign(r) * theta + phi_offset) + hx
         y = abs(r) * np.sin(-np.sign(r) * theta + phi_offset) + hy
-        z = 2 * np.pi * r * np.tan(np.pi * theta_i / 180) * theta / (2 * np.pi) + hz
+        z = abs(r) * np.tan(theta_i) * theta + hz
 
         return x, y, z

melp/vis/toyGen.py
@@ -19,7 +19,7 @@ class ToyEventGenerator:
         py = self.pt * np.sin(theta)
 
         pid = self.particle_type
-        self.id += 1
+        self.id -= 1
         return Trajectory(id=self.id,
                           vx=vx,
                           vy=vy,

melp/vis/visualizer.py
@@ -38,6 +38,9 @@ class Visualizer:
         self.color_tile = "gray"
         self.color_electrons = "blue"
         self.color_positrons = "red"
+        self.color_muons = "green"
+        self.color_photon = "yellow"
+        self.color_std = "gray"
         self.f_size = 25
 
         self.dpi = 250
@@ -128,6 +131,12 @@ class Visualizer:
             if traj.mother == -1:
                 self.trajectories.append(traj.id)
 
+    def select_only_particle_types(self, pids: list):
+        self.trajectories = []
+        for traj in self.traj_objects:
+            if traj.get_particle_type() in pids:
+                self.trajectories.append(traj.id)
+
     def set_plt_options_2d(self, **kwargs):
         # -------------------------
         # Set plt options for selected settings
@@ -153,26 +162,21 @@ class Visualizer:
         fig, ax_arr = plt.subplots(1, 2, figsize=(30, 10), dpi=self.dpi)
 
         ax = ax_arr[0]
-        for index_current in self.trajectories:
-            # ax.scatter(self.list_traj_vx[index_current], self.list_traj_vy[index_current])
-            ax.scatter(np.array(x_t), np.array(y_t), marker="o", color="r", linewidths=3)
-            # ax.arrow(self.list_traj_vx[index_current],
-            #         self.list_traj_vy[index_current],
-            #         self.list_traj_px[index_current],
-            #         self.list_traj_py[index_current], length_includes_head=True, head_width=5, head_length=5)
+
+        for traj in self.traj_objects:
+            if traj.id not in self.trajectories:
+                continue
+            ax.arrow(traj.vx, traj.vy, traj.px, traj.py,
+                     length_includes_head=True, head_width=3, head_length=3)
+
         for traj in self.traj_objects:
             if traj.id not in self.trajectories:
                 continue
             x, y, z = traj.get_helix_path()
-            color = "gray"
-            if traj.get_particle_type() == 1:
-                color = "red"
-            elif traj.get_particle_type() == 2:
-                color = "blue"
-            elif traj.get_particle_type() == 0:
-                color = "yellow"
-            ax.plot(x, y, color=color)
 
+            ax.plot(x, y, color=self._color_for_particle_(traj.get_particle_type()))
+
+        ax.scatter(np.array(x_t), np.array(y_t), marker="o", color="r", linewidths=3)
         ax.scatter(x_t_geom, y_t_geom, marker=".", alpha=0.1, color=self.color_tile)
 
         circle2 = plt.Circle((0, 0), 19, color='black', alpha=0.1)
@@ -185,25 +189,20 @@ class Visualizer:
         ax.set_xlabel("x", fontsize=self.f_size)
 
         ax = ax_arr[1]
-        for index_current in self.trajectories:
-            # ax.scatter(self.list_traj_vz[index_current], self.list_traj_vx[index_current])
-            ax.scatter(np.array(z_t), np.array(x_t), marker="o", color="r", linewidths=3)
-            # ax.arrow(self.list_traj_vz[index_current],
-            #         self.list_traj_vx[index_current],
-            #         self.list_traj_pz[index_current],
-            #         self.list_traj_px[index_current], length_includes_head=True, head_width=5, head_length=5)
+
+        for traj in self.traj_objects:
+            if traj.id not in self.trajectories:
+                continue
+            ax.arrow(traj.vz, traj.vx, traj.pz, traj.px,
+                     length_includes_head=True, head_width=3, head_length=3)
+
         for traj in self.traj_objects:
             if traj.id not in self.trajectories:
                 continue
             x, y, z = traj.get_helix_path()
-            color = "gray"
-            if traj.get_particle_type() == 1:
-                color = "red"
-            elif traj.get_particle_type() == 2:
-                color = "blue"
-            elif traj.get_particle_type() == 0:
-                color = "yellow"
-            ax.plot(z, x, color=color)
+            ax.plot(z, x, color=self._color_for_particle_(traj.get_particle_type()))
+
+        ax.scatter(np.array(z_t), np.array(x_t), marker="o", color="r", linewidths=3)
         ax.scatter(z_t_geom, x_t_geom, marker=".", alpha=0.1, color=self.color_tile)
 
         ax.axis('equal')
@@ -231,14 +230,7 @@ class Visualizer:
             if traj.id not in self.trajectories:
                 continue
             x, y, z = traj.get_helix_path()
-            color = "gray"
-            if traj.get_particle_type() == 1:
-                color = "red"
-            elif traj.get_particle_type() == 2:
-                color = "blue"
-            elif traj.get_particle_type() == 0:
-                color = "yellow"
-            ax.plot(z, x, y, color=color)
+            ax.plot(z, x, y, color=self._color_for_particle_(traj.get_particle_type()))
 
         # add tile hits
         ax.scatter(np.array(z_t), np.array(x_t), np.array(y_t), marker="o", color="r", linewidths=3)
@@ -288,3 +280,16 @@ class Visualizer:
                     z_t.append(self.mu3e_detector.TileDetector.tile[self.list_tilehit_tile[index]].pos[2])
 
         return x_t, y_t, z_t
+
+    def _color_for_particle_(self, pid: int):
+        color = self.color_std
+        if pid == 1:
+            color = self.color_positrons
+        elif pid == 2:
+            color = self.color_electrons
+        elif pid == 0:
+            color = self.color_photon
+        elif pid == 3 or pid == 4:
+            color = self.color_muons
+
+        return color